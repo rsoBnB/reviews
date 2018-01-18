8f27b8bfd02bee2ed3c13c9480298667f1dcf89c
Greg <stravs.grega@gmail.com>
2018-01-18 19:59:52 +0100

    Healthy fix

--- api/src/main/java/si/fri/rso/rsobnb/reviews/api/v1/resources/DemoResource.java
+++ api/src/main/java/si/fri/rso/rsobnb/reviews/api/v1/resources/DemoResource.java
@@ -25,16 +25,6 @@ public class DemoResource {
     @Inject
     private RestProperties restProperties;
 
-    @GET
-    @Path("instanceid")
-    public Response getInstanceId() {
-
-        String instanceId =
-                "{\"instanceId\" : \"" + EeRuntime.getInstance().getInstanceId() + "\"}";
-
-        return Response.ok(instanceId).build();
-    }
-
     @POST
     @Path("healthy")
     public Response setHealth(Boolean healthy) {
@@ -52,37 +42,6 @@ public class DemoResource {
         }
 
         return Response.status(Response.Status.OK).build();
-    }
-
-    @GET
-    @Path("info")
-    public Response info() {
-
-        JSONObject json = new JSONObject();
-
-        JSONArray clani = new JSONArray();
-        clani.put("jm1234");
-
-        JSONArray mikrostoritve = new JSONArray();
-        mikrostoritve.put("http://35.189.96.119:8081/v1/orders");
-
-        JSONArray github = new JSONArray();
-        github.put("https://github.com/jmezna/rso-customers");
-
-        JSONArray travis = new JSONArray();
-        travis.put("https://travis-ci.org/jmezna/rso-customers");
-
-        JSONArray dockerhub = new JSONArray();
-        dockerhub.put("https://hub.docker.com/r/jmezna/rso-customers");
-
-        json.put("clani", clani);
-        json.put("opis_projekta", "Nas projekt implementira aplikacijo za oddajo nepremicnin.");
-        json.put("mikrostoritve", mikrostoritve);
-        json.put("github", github);
-        json.put("travis", travis);
-        json.put("dockerhub", dockerhub);
-
-        return Response.ok(json.toString()).build();
     }
 
     private long fibonacci(int n) {
